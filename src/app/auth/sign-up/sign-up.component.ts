4dc8eb9c48cea7a893b6bb7de58f896c6ea635ab
Jamal40 <Jamal_Ali@outlook.com>
2021-08-31 15:52:08 +0200

    Setting up the project

---
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css'],
+})
+export class SignUpComponent implements OnInit {
+  authForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    passwordConfirmation: new FormControl(''),
+  });
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
